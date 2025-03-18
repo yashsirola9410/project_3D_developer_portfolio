f09222633d00b32fd4718e36eeebaa11af87fb7b
yashsirola9410 <yashtechid9410@gmail.com>
2025-03-19 01:00:45 +0530

    hxvdsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import { BrowserRouter } from "react-router-dom";
+import Tilt from "react-parallax-tilt";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-          <Hero />
+          {/* Wrapped Hero section with Tilt */}
+          <Tilt>
+            <Hero />
+          </Tilt>
         </div>
         <About />
         <Experience />
         <Tech />
         <Works />
         <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
